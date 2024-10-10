7f632098364485ad6c304fda190b76ffdaf854f5
Oleksiy Levkovskyy <lex@sojourns.io>
2024-10-10 15:25:26 -0400

    Updated configuration and error handling

--- serner.js
+++ serner.js
@@ -6,6 +6,13 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
+
+// Add this error handling middleware
+server.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 server.use(router);
 
 server.listen(port, () => {
